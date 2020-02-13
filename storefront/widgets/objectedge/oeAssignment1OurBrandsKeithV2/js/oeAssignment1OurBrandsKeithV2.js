1059fea2939a647640e8ca1d7b2c71c3cb8d15d3
kinfo <keith.sarate@objectedge.com>
2020-02-13 11:48:37 -0300

    OET-87: Create widget oeAssignment1OurBrandsV2

---
+++ storefront/widgets/objectedge/oeAssignment1OurBrandsKeithV2/js/oeAssignment1OurBrandsKeithV2.js
@@ -0,0 +1,80 @@
+/**
+ * @fileoverview oeAssignment1OurBrandsKeith.
+ *
+ * @author keith.sarate@objectedge.com
+ */
+define(
+  //-------------------------------------------------------------------
+  // DEPENDENCIES
+  //-------------------------------------------------------------------
+  ['knockout', 'ccLogger', 'ccRestClient', 'ccConstants'],
+
+  //-------------------------------------------------------------------
+  // MODULE DEFINITION
+  //-------------------------------------------------------------------
+  function (ko, ccLogger, ccRestClient, ccConstants) {
+
+    "use strict";
+
+    var collectionIds = [
+      'keith_centenial',
+      'keith_gbc',
+      'keith_greenball',
+      'keith_kanat'
+    ]
+
+    //CLASS
+    function brand(title, image) {
+
+      this.title = title || '';
+      this.image = image;
+
+      return this;
+    }
+
+    function getCollection(id, callback) {
+
+      debugger
+
+      ccRestClient.request('/ccstore/v1/collections/' + id,
+        {},
+        callback.bind(this),
+        function (error) {
+          //callback(null, error);
+          ccLogger.error('Error trying to fretch product information:', error);
+        }
+      );
+    }
+
+    return {
+
+      /**
+       * This function will run just on time when the widget was loaded the first time
+       */
+
+      onLoad: function (widget) {
+
+        ccLogger.info("[OE][onLoad] Loading oeAssignment1OurBrandsKeith");
+
+        widget.brands = ko.observableArray();
+
+        collectionIds.map(function (id) {
+
+          getCollection(id, function(collection){
+
+            widget.brands.push(new brand(collection.displayName,
+                                         collection.categoryImages[0].path));
+          });
+        });
+
+      },
+
+      /**
+       * This function will run whe the widget was loaded and every page change
+       */
+      beforeAppear: function (page) {
+
+      }
+    };
+  }
+);
